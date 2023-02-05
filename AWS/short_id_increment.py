446fe76fc5bc9eae26e2e5ec5ed000f140b419fe
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2023-02-05 19:38:42 +0530

    changes

--- AWS/short_id_increment.py
+++ AWS/short_id_increment.py
@@ -9,6 +9,14 @@ def increment_short_id(current_id, max_id):
     return short_id
 
 
-current_id = current_short_id.current_short_id
-short_id = increment_short_id(current_id, 5)
+def main():
+    current_id = current_short_id.current_short_id
+    short_id = increment_short_id(current_id, 9)
+    return short_id
+    
+
+if __name__ == '__main__':
+    main()
+
+
 
